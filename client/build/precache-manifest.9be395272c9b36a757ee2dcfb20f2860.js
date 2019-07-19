0fe2de82faeb7314006ae4a125bbd48f7723efe3
Rachel Tezza <racheletezza@gmail.com>
2019-07-19 12:04:24 -0400

    Client folder minus node modules added

---
+++ client/build/precache-manifest.9be395272c9b36a757ee2dcfb20f2860.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b14ba862c125dbee662cc4d31099da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "444dbb10a82a39f70097",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "444dbb10a82a39f70097",
+    "url": "/static/js/main.d419a5e9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
